43874ba9588f793e7b6bdf1e941f543e40fadf03
Delphine <delphine.pva@gmail.com>
2020-11-19 22:17:49 +0100

    Testing React Bootstrap

--- src/Components/Gods/DropDown.js
+++ src/Components/Gods/DropDown.js
@@ -11,17 +11,18 @@ const Bardropdown = (props) => {
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
       <DropdownToggle caret>
-        Dropdown
+        All gods skills
         </DropdownToggle>
       <DropdownMenu>
-        <DropdownItem header>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
+        {/* <DropdownItem header>Header</DropdownItem>
+        <DropdownItem>War</DropdownItem>
         <DropdownItem text>Dropdown Item Text</DropdownItem>
         <DropdownItem disabled>Action (disabled)</DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem>Foo Action</DropdownItem>
-        <DropdownItem>Bar Action</DropdownItem>
-        <DropdownItem>Quo Action</DropdownItem>
+        <DropdownItem divider /> */}
+        <DropdownItem>Art</DropdownItem>
+        <DropdownItem>Beauty</DropdownItem>
+        <DropdownItem>Love</DropdownItem>
+        <DropdownItem>War</DropdownItem>
       </DropdownMenu>
     </Dropdown>
   );

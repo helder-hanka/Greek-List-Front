42286e6a364107f3f9c7b2e465c8d888931d9a0d
Delphine <delphine.pva@gmail.com>
2020-11-19 23:34:45 +0100

    Create two new files Cards.js and Card.css

---
+++ src/Components/Gods/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Cards.css';
+
+const Cards = ({ god }) => {
+        return (
+            <div className="container">
+                <Card className="style">
+                    <CardImg variant="top" src={god.avatar} alt={god.name} />
+                    <CardBody>
+                        <CardTitle tag="h5">{god.name}</CardTitle>
+                        <CardText>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </CardText>
+                        <Button variant="primary">View God</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+};
+
+export default Cards;

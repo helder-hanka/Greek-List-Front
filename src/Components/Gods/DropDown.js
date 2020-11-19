12fddafe6e6684c537ff48f4ae7bd8b908f442e5
Anthony Portier <anthonynicolasportier@gmail.com>
2020-11-20 00:35:39 +0100

    change name hooks

--- src/Components/Gods/DropDown.js
+++ src/Components/Gods/DropDown.js
@@ -5,16 +5,16 @@ import './dropdown.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Bardropdown = (props) => {
-  const [button, setButton] = useState(false);
-  const inverseButton = () => setButton(prevState => !prevState);
-
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen(prevState => !prevState);
-
+  
+  const [listgod, setListgod] = useState(false);
+  const togglelistgod = () => setListgod(prevState => !prevState);
+  
+  const [competency, setCompetency] = useState(false);
+  const togglelistcompetency = () => setCompetency(prevState => !prevState);
   return (
     <div className = "button" >
       <h4>Select the skill : </h4>
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+    <Dropdown isOpen={listgod} toggle={togglelistgod}>
       <DropdownToggle caret>
         All gods 
         </DropdownToggle>
@@ -28,7 +28,7 @@ const Bardropdown = (props) => {
       </DropdownMenu>
     </Dropdown>
 
-      <Dropdown isOpen={button} toggle={inverseButton}>
+      <Dropdown isOpen={competency} toggle={togglelistcompetency}>
         <DropdownToggle caret>
           competency gods
           </DropdownToggle>

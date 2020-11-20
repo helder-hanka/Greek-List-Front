79669ac8c7414a5368221570c5f53ccbf3e3eee3
helder-hanka <hfernandes238@gmail.com>
2020-11-20 11:20:18 +0100

    fixing mission table

--- src/Components/UIGods/GodList.js
+++ src/Components/UIGods/GodList.js
@@ -12,20 +12,57 @@ const  GodList = () => {
         {   
             user_id: "5",
             title: "Kill Paul",
-            status_id: 1,
+            status_id: 4,
             name: "refused"
         }
     ]
     
     const [ userDatas, setUserDatas ] = useState(myArrayUser)
     const { name, status_id, title } = userDatas[0];
+    const [ buttonColors, setButtonColors] = useState({
+        accepted : null,
+        refused: null,
+        finish: null
+    })
+
     console.log(userDatas)
 
     useEffect(() => {
         
     }, [userDatas[0]])
 
-    
+    useEffect(()=>{
+        switch(status_id) {
+            case 1 :
+                setButtonColors({
+                    accepted : 'success',
+                    refused: 'danger',
+                    finish: 'secondary'
+                })
+                break
+            case 2 :
+                setButtonColors({
+                    accepted : 'success',
+                    refused: 'secondary',
+                    finish: 'secondary'
+                })
+                break
+            case 3 :
+                setButtonColors({
+                    accepted : 'secondary',
+                    refused: 'danger',
+                    finish: 'secondary'
+                })
+                break
+            case 4 :    
+                setButtonColors({
+                    accepted : 'secondary',
+                    refused: 'secondary',
+                    finish: 'success'
+                })
+                break
+        }
+    },[status_id])
     
     const onSubmitMission = (e) => {
         e.preventDefault();
@@ -46,29 +83,6 @@ const  GodList = () => {
 
     // done: Cfait = 4
 
-    const test = (e) => {
-        if (e === 1 && e !== 2 && e !== 3) {
-            return true
-        } else if (e === 2) {
-            return true
-        }
-    }
-
-    // const displayAccepted = (status_id === 2 || !status_id === 4 ||  status_id === 1) && (status_id === 3) ? `${"success"}` : `${"secondary"}`; 4
-    const displayAccepted = test(status_id) ? `${"success"}` : `${"secondary"}`;
-    console.log('Accepted', displayAccepted)
-    // const displayFinish  = displayStatus(status_id) === "done" ? `${"success"}` : `${"secondary"}`;
-
-    const displayRefused = test(status_id) ?`${"secondary"}` : `${"danger"}` ;
-
-    // const displayRefused = status_id === 3 ? `${"danger"}` :`${"secondary"}` ;
-    console.log('Refused', displayRefused)
-
-    // const displayPending = displayStatus(status_id) === "pending" ? `${"success"}` : `${"secondary"}`;
-    // const displayFinish  = status_id === 4 ?`${"success"}` : `${"secondary"}`;
-    // console.log('Finish', displayFinish)
-    // const displayLetter = finish ? 'A' : '?';
-
     const display = userDatas.map((userData, index) => 
         <div key={index}>
             <Table striped>
@@ -85,11 +99,11 @@ const  GodList = () => {
                     <tr>
                         <td >{userData.title}</td>
                         <td>{userData.status_id}</td>
-                        <td><ButtonToggle color={displayAccepted}
+                        <td><ButtonToggle color={buttonColors.accepted}
                         name='accepted' type="submit" onClick={(e) => onSubmitMission(e)}>V</ButtonToggle></td>
-                        <td><ButtonToggle color={displayRefused} 
+                        <td><ButtonToggle color={buttonColors.refused} 
                         name="refused" type="submit" onClick={(e) => onSubmitMission(e)}>X</ButtonToggle></td>
-                        <td><ButtonToggle //color={displayFinish} 
+                        <td><ButtonToggle color={buttonColors.finish} 
                         name="done" type="submit" onClick={(e) => onSubmitMission(e)}>V</ButtonToggle></td>
                     </tr>
                 </tbody>

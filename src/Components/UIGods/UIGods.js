a43706928b625c8bc0c12a914a0ffb5df4d42ac3
Fredy <fredy.ly.wcs@gmail.com>
2020-11-20 02:45:19 +0100

    cleaning code

--- src/Components/UIGods/UIGods.js
+++ src/Components/UIGods/UIGods.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+// import qs from 'qs';
 import GodList from './GodList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,47 +10,31 @@ import './UIGods.css';
 
 const UIGods = () => {
 
-    const myArrayGod =
-        {
+    const myArrayGod = {
             id:'0',
             name: 'Fredy'
-        }
-
-        // get Axios
-    const myArrayUser = [
-        {   
-            user_id: "5",
-            title: "Kill Paul",
-            status_id: "2",
-            finish: true
-
-        }
-    ]
+    }
 
     const [ god, setGod ] = useState(myArrayGod);
-    const { id, name } = god;
-    console.log(god)
-
-    const [ userDatas, setUserDatas ] = useState(myArrayUser)
-    const { title, status_id, finish } = userDatas;
-    console.log(userDatas)
-    
+    const { //id,
+         name } = god;
+    // console.log(god)
 
     const handleChange = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setGod({...god, [e.target.name]: e.target.value})
     }
 
-    const onSubmitForm = (e) => {
-        e.preventDefault();
-        console.log(id, name)
-        const config = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(god),
-        };
+    // const onSubmitForm = (e) => {
+    //     e.preventDefault();
+    //     console.log(id, name)
+    //     const config = {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //         body: qs.stringify(god),
+    //     };
 
         // const url = "https://post-a-form.herokuapp.com/api/employees/";
         // fetch(url, config)
@@ -65,19 +50,13 @@ const UIGods = () => {
         //     alert('There was an error when adding the employee.');
         // });
         
-    }
-
-    const displayUserData = userDatas.map(userData => <GodList {...userData}/>)
-
-    const onSubmitMission = (e) => {
-        e.preventDefault();
-
-    }
-
+    // }
+    
     return (
         <div>
             <p>Name: {name}</p>
-            <Form onSubmit={onSubmitForm}>
+            <Form //onSubmit={onSubmitForm}
+            >
                 <FormGroup>
                 <Label check >
                 <Col sm={10}>
@@ -91,9 +70,8 @@ const UIGods = () => {
                 </Label>
                 <Button type="submit">Modify</Button>
                 </FormGroup>                
-
             </Form>
-            {displayUserData}
+            <GodList />
         </div>
     )
 }
